aca4d684106fb1a947e2691a3f9c86a5df560de0
Tim Freeman <tukerty@Gmail.com>
2018-05-19 21:47:50 +0300

    created map and player

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,8 +1,13 @@
 <template>
-  <div id="map_component">
+<div id="map_component">
     <canvas id="map" width="640" height="480">
     </canvas>
     <div class="data">{{this.player}}</div>
+    <button @click="movePlayer(1)">top</button>
+    <button @click="movePlayer(2)">top-right</button>
+    <button @click="movePlayer(5)">right</button>
+    <button @click="movePlayer(7)">bot</button>
+    <button @click="movePlayer(3)">left</button>
   </div>
 </template>
 
@@ -11,82 +16,96 @@ export default {
   name: 'Map',
   data () {
     return {
-      player: {},
-      map: {},
-    }
-  },
-  mounted () {
-    var canvas = document.getElementById("map");
-    var context = canvas.getContext("2d");
-    this.initMap(canvas,context)
-    this.map.generate()
-    this.renderMap()
-    this.initPlayer()
-    this.player.update()
-  },
-  methods: {
-    initMap: function (canvas, context) {
-      this.map = {
-        canvas: canvas,
-        context: context,
-        generate: function() {
-          var mapArray = new Array(32)
-          for (var i = 0; i < 32; i++) {
-            mapArray[i] = new Array(24)
-          }
-          for (i = 0; i < 32; i++) {
-            for (var j = 0; j < 24; j++) {
-              mapArray[i][j] = Math.floor(Math.random() * 2)
-            }
-          }
-          return mapArray
-        },
-        mapArray: generate(),
-      }
-    },
-    renderMap: function(){
-      var canvas = document.getElementById('map')
-      var context = canvas.getContext('2d')
-      for (var i = 0; i<32; i++){
-        for (var j = 0; j<24; j++){
-          if (this.mapArray[i][j]){
-              context.fillStyle = "blue";
-              context.fillRect(i*20, j*20, 20, 20);
-          }
-          else{
-              context.fillStyle = "black";
-              context.fillRect(i*20, j*20, 20, 20);
-          }
-        }
-      }
-    },
-    updateMap: function() {
-    },
-    initPlayer: function(){
-      this.player = {
+      mapArray: new Array(32),
+      player: {
         width: 20,
         height: 20,
-        color: "red",
+        color: 'red',
+        isWalk: false,
         speed: {
           x: 0,
           y: 0
         },
         position: {
-          x:0,
-          y:0
+          x: 0,
+          y: 0
         },
         aPos: {
-          x:0,
-          y:0
-        },
-        update: function() {
-          map.context.fillStyle = this.color;
-          ctx.fillRect(this.x, this.y, this.width, this.height);
+          x: 0,
+          y: 0
+        }
+      }
+    }
+  },
+  mounted () {
+    this.initMap()
+    this.generateMap()
+    this.renderMap()
+    this.renderPlayer()
+  },
+  methods: {
+    initMap: function () {
+      this.canvas = document.getElementById('map')
+      this.context = this.canvas.getContext('2d')
+      this.interval = setInterval(this.updateMap, 20)
+    },
+    generateMap: function () {
+      for (var i = 0; i < 32; i++) {
+        this.mapArray[i] = new Array(24)
+      }
+      for (i = 0; i < 32; i++) {
+        for (var j = 0; j < 24; j++) {
+          this.mapArray[i][j] = Math.floor(Math.random() * 2)
         }
       }
     },
-    renderPlayer: function(){
+    renderMap: function () {
+      for (var i = 0; i < 32; i++) {
+        for (var j = 0; j < 24; j++) {
+          if (this.mapArray[i][j]) {
+            this.context.fillStyle = 'blue'
+            this.context.fillRect(i * 20, j * 20, 20, 20)
+          } else {
+            this.context.fillStyle = 'black'
+            this.context.fillRect(i * 20, j * 20, 20, 20)
+          }
+        }
+      }
+    },
+    clearMap: function () {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    },
+    updateMap: function () {
+      this.clearMap()
+      this.renderMap()
+      this.updatePlayer()
+      this.renderPlayer()
+    },
+    renderPlayer: function () {
+      this.context.fillStyle = this.player.color
+      this.context.fillRect(this.player.position.x, this.player.position.y, this.player.width, this.player.height)
+    },
+    movePlayer: function (direction) {
+      if (!this.player.isWalk) {
+        this.player.isWalk = true
+        var n = direction.toString(3)
+        if (n.length === 1) { n = '0' + n }
+        this.player.speed.y = (n[0] - 1)
+        this.player.speed.x = (n[1] - 1)
+      }
+    },
+    updatePlayer: function () {
+      this.player.position.x += this.player.speed.x
+      this.player.position.y += this.player.speed.y
 
+      if ((Math.abs(this.player.aPos.x - this.player.position.x) === 20) || (Math.abs(this.player.aPos.y - this.player.position.y) === 20)) {
+        this.player.isWalk = false
+        this.movePlayer(4)
+        console.log(this.player.isWalk)
+        this.player.aPos.x = this.player.position.x
+        this.player.aPos.y = this.player.position.y
+        this.player.isWalk = false
+      }
     }
   }
 }
